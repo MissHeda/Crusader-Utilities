d88d6b2783c2aa045d23dea7fd4dd82e5113269f
Mia <95135995+MissHeda@users.noreply.github.com>
2024-03-26 13:13:55 +0100

    Fix done

---
+++ addons/utilities/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT utilities
+#define COMPONENT_BEAUTIFIED Crusader - Utilities
+#include "\z\crusader\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_UTILITIES
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_UTILITIES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_UTILITIES
+#endif
+
+#include "\z\crusader\addons\main\script_macros.hpp"

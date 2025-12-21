52cc553cf841afa22175a573d6cc72b5fe02920e
Mia <95135995+MissHeda@users.noreply.github.com>
2025-12-21 18:33:31 +0100

    Add playing video regardless of static Background param
    Credit: Puotek https://steamcommunity.com/id/puotekpl/

--- addons/utilities/CfgMainMenuGui.hpp
+++ addons/utilities/CfgMainMenuGui.hpp
@@ -1,10 +1,12 @@
 // Define images to mass edit
-#define Startup_Logo                    QPATHTOF(assets\ui\Arma_x_Crusader.paa);
-#define Main_Menu_Logo                  QPATHTOF(assets\ui\CrusaderLogo.paa);
-#define Main_Menu_Background            QPATHTOF(assets\ui\CrusaderMainMenu.jpg);
-#define Multiplayer_Menu_Background     QPATHTOF(assets\ui\CrusaderMultiplayerMenu.jpg);
+#define Startup_Logo                    QPATHTOF(assets\ui\Arma_x_Crusader.paa)
+#define Main_Menu_Logo                  QPATHTOF(assets\ui\CrusaderLogo.paa)
+#define Main_Menu_Background            QPATHTOF(assets\ui\CrusaderMainMenu.jpg)
+#define Multiplayer_Menu_Background     QPATHTOF(assets\ui\CrusaderMultiplayerMenu.jpg)
+#define Main_Menu_Video                 QPATHTOF(assets\mainmenu.VR\Crusader_Intro.ogv)
 
 // Get all classes the file needs
+class RscVideo;
 class Attributes;
 class RscStandardDisplay;
 class RscProgress;
@@ -59,17 +61,12 @@ class RscButtonMenuSteam;
 class RscButtonTextOnly;
 
 // Override additional arma loading image that gets used if -skipIntro is not set. (disables popup)
-class RscDisplayLoadMission: RscStandardDisplay
-{
-    class controls
-    {
-        class LoadingStart: RscControlsGroup
-        {
-            class controls
-            {
-                class Logo: RscPictureKeepAspect
-                {
-                    text = Startup_Logo
+class RscDisplayLoadMission: RscStandardDisplay {
+    class controls {
+        class LoadingStart: RscControlsGroup {
+            class controls {
+                class Logo: RscPictureKeepAspect {
+                    text = Startup_Logo;
                     x = "0.33375 * safezoneW";
                     y = "0.29 * safezoneH";
                     w = "0.3325 * safezoneW";
@@ -81,16 +78,11 @@ class RscDisplayLoadMission: RscStandardDisplay
 };
 
 // Override initial arma loading
-class RscDisplayStart: RscStandardDisplay
-{
-    class controls
-    {
-        class LoadingStart: RscControlsGroup
-        {
-            class controls
-            {
-                /*class Noise: RscPicture // Background picture
-                {
+class RscDisplayStart: RscStandardDisplay {
+    class controls {
+        class LoadingStart: RscControlsGroup {
+            class controls {
+                /*class Noise: RscPicture { // Background picture
                     idc = 1201;
                     text = "";
                     x = "0 * safezoneW";
@@ -98,9 +90,8 @@ class RscDisplayStart: RscStandardDisplay
                     w = "1 * safezoneW";
                     h = "1 * safezoneH";
                 };*/
-                class Logo: RscPictureKeepAspect // Arma logo
-                {
-                    text = Startup_Logo
+                class Logo: RscPictureKeepAspect { // Arma logo
+                    text = Startup_Logo;
                     x = "0.33375 * safezoneW";
                     y = "0.29 * safezoneH";
                     w = "0.3325 * safezoneW";
@@ -112,17 +103,12 @@ class RscDisplayStart: RscStandardDisplay
 };
 
 // Override additional arma loading image that gets used if -skipIntro is not set. (disables popup)
-class RscDisplayNotFreeze: RscStandardDisplay
-{
-    class controls
-    {
-        class LoadingStart: RscControlsGroup
-        {
-            class controls
-            {
-                class Logo: RscPictureKeepAspect
-                {
-                    text = Startup_Logo
+class RscDisplayNotFreeze: RscStandardDisplay {
+    class controls {
+        class LoadingStart: RscControlsGroup {
+            class controls {
+                class Logo: RscPictureKeepAspect {
+                    text = Startup_Logo;
                     x = "0.33375 * safezoneW";
                     y = "0.29 * safezoneH";
                     w = "0.3325 * safezoneW";
@@ -133,55 +119,53 @@ class RscDisplayNotFreeze: RscStandardDisplay
     };
 };
 
-// Disables the Main Menu Ace Version popup at the bottem right & the Project Opfor logo
+// Disables the Main Menu Ace Version popup at the bottem right, the Project Opfor logo, enable video to play on load
 class Extended_DisplayLoad_EventHandlers {
     class RscDisplayMain {
         Crusader_Disable_ACE_loadMainMenuBox = "_this select 0 displayCtrl 80090 ctrlShow False";
         Crusader_Disable_PO_ProjectOPFORLogo = "_this select 0 displayCtrl 1299 ctrlShow False";
-    };
-};
-
-// Overrides the main menu background image that gets set when users has -skipIntro as a launch parameter.
-class RscTitles 
-{
-    class RscDisplayMainMenuBackground
-    {
-        class Controls
-        {
-            class Picture: RscPicture
-            {
-                text = Main_Menu_Background
-                h = "safeZoneH";
-            };
-        };
+        Crusader_Play_MainMenuVideo = "with uiNamespace do {[""playVideo"", [((_this select 0) displayCtrl 213702), true], """"] call RscDisplayMain_script}";
     };
 };
 
 // Disables the middel spotlight box
-class RscMainMenuSpotlight: RscControlsGroupNoScrollbars
-{
+class RscMainMenuSpotlight: RscControlsGroupNoScrollbars {
     show = 0;
     onLoad = "";
 };
 
-// Disables arma & mod infos
-class RscDisplayMain: RscStandardDisplay 
-{
-/*  class controlsBackground // If you add another class via picture here it overrides everything in the main menu even loaded missions. Usefull if only a image should be displayed without any video via mission.
-    {
-        class Picture_Custom_Crusader: RscPicture
-        {
-            idc = -1;
-            text = Main_Menu_Background
-            x = "safezoneX";
-            y = "safezoneY";
-            w = "safezoneW";
-            h="safeZoneH";
+// Disables arma & mod infos | creates video playback & sets a backupimage if it somehow does not load
+class RscDisplayMain: RscStandardDisplay {
+    class ControlsBackground {
+        delete Picture;
+        class Background : RscControlsGroupNoScrollbars {
+            idc = 213702;
+            x = "safeZoneX";
+            y = "safeZoneY";
+            h = "safeZoneH";
+            w = "safeZoneW";
+            class Controls {
+                class Picture : RscPictureKeepAspect {
+                    idc = 1023;
+                    text = Main_Menu_Background;
+                    x = 0;
+                    y = 0;
+                    h = "safeZoneH";
+                    w = "safeZoneW";
+                };
+                class Video : RscVideo {
+                    idc = 1024;
+                    text = Main_Menu_Video;
+                    x = 0;
+                    y = 0;
+                    h = "safeZoneH + 11 * pixelH";
+                    w = "safeZoneW";
+                };
+            };
         };
     };
-*/
-    class controls
-    {
+
+    class controls {
         class BackgroundSpotlight: RscPicture 
         {
             show=0;
@@ -258,9 +242,9 @@ class RscDisplayMain: RscStandardDisplay
         {
             show = 0;
         };
-        class Logo: RscActivePicture // Remove default arma logo and replace with custom one
-        { 
-            text = Main_Menu_Logo
+        
+        class Logo: RscActivePicture { // Remove default arma logo and replace with custom one
+            text = Main_Menu_Logo;
             tooltip = "";
             //tooltip = CSTRING(MAINMENU_ArmaLogo_ReplacementIcon_DESC);
             //url = "https://xn--kommandokrfte-crusader-94b.de/";
@@ -271,25 +255,39 @@ class RscDisplayMain: RscStandardDisplay
             w = "1.4 *  5 *  (pixelW * pixelGrid * 2)"; // Modify width of the image (change first value (1.4))
             h = "1.4 *  5 *  (pixelH * pixelGrid * 2)"; // Modify height of the image (change first value (1.4))
         };
-        class GroupSingleplayer: RscControlsGroupNoScrollbars // Call Controls from GroupSingleplayer to inherit it in GroupMultiplayer
-        {
+        
+        class GroupSingleplayer: RscControlsGroupNoScrollbars { // Call Controls from GroupSingleplayer to inherit it in GroupMultiplayer
             class Controls;
         };
-        class GroupMultiplayer: GroupSingleplayer
-        {
+        
+        class GroupMultiplayer: GroupSingleplayer {
             h = "(5 *     1.5) *     (pixelH * pixelGrid * 2)"; // This sets the lenght of the displayed multiplayer list. The first value stands for the number of fields that can be filled. Default 3, here 5 due to us adding 2 options.
-            class Controls: Controls
-            {
+            class Controls: Controls {
                 class Campaigns;
-                class join_CrusaderServer: Campaigns // Custom join button that is shown below normal multiplayer buttons
-                {
+                class join_CrusaderServer: Campaigns { // Custom join button that is shown below normal multiplayer buttons
                     idc = -1;
                     text = CSTRING(MAINMENU_ArmaServer_Connect);
                     tooltip = CSTRING(MAINMENU_ArmaServer_Connect_DESC);
                     y = "(3 *     1.5) *     (pixelH * pixelGrid * 2) +     (pixelH)";
                     onbuttonclick = "connectToServer ['162.55.107.229', 2302, ''];";
                 };
+                class join_CrusaderTeamSpeak: join_CrusaderServer { // Custom join button for teamspeak
+                    text = CSTRING(MAINMENU_TeamSpeak_Connect);
+                    tooltip = CSTRING(MAINMENU_TeamSpeak_Connect_DESC);
+                    url = "https://puotek.github.io/ts3connect/#162.55.107.229";
+                    y = "(4 *     1.5) *     (pixelH * pixelGrid * 2) +     (pixelH)";
+                    onbuttonclick = "";
+                };
             };
+        };
+
+        class TitleSession;
+        class TitleTeamspeak: TitleSession { // Additional Teamspeak join icon
+            onbuttonclick = "";
+            tooltip = CSTRING(MAINMENU_TeamSpeak_Connect_DESC);
+            url = "https://puotek.github.io/ts3connect/#162.55.107.229";
+            x = "safezoneX + safezoneW - 3 * 2 * (pixelW * pixelGrid * 2)";
+            text = "a3\modules_f_curator\data\portraitradio_ca.paa";
         };
     };
 };
@@ -306,7 +304,7 @@ class RscDisplayMultiplayer: RscStandardDisplay
                 class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
             };
         };
@@ -325,7 +323,7 @@ class RscDisplayFilter: RscStandardDisplay
                 class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
 			};
 		};
@@ -344,7 +342,7 @@ class RscDisplayPassword: RscStandardDisplay
 				class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
 			};
 		};
@@ -363,7 +361,7 @@ class RscDisplayIPAddress: RscStandardDisplay
 				class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
 			};
 		};
@@ -382,7 +380,7 @@ class RscDisplaySetupServer: RscStandardDisplay
 				class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
 			};
 		};
@@ -401,59 +399,16 @@ class RscDisplayQuickPlay: RscStandardDisplay
 				class Background: RscPicture // Changed RscText to RscPicture to use a picture
                 {
                     idc = 114999;
-                    text = Multiplayer_Menu_Background
+                    text = Multiplayer_Menu_Background;
                 };
 			};
 		};
     };
 };
 
-// Override cutsene for maps
-class CfgWorlds 
-{
-    class CAWorld;
-    class Altis : CAWorld 
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-    
-    class Stratis : CAWorld 
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-
-    class Enoch : CAWorld 
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-
-    class VR : CAWorld 
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-
-    class gm_weferlingen_summer : CAWorld
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-
-    class hebontes : Stratis
-    {
-        cutscenes[] = {"Main_Menu_Intro"};
-    };
-
-    initWorld = "VR";
-    demoWorld = "VR";
-};
-
-// Define a new mission to use in cutsene
-class CfgMissions
-{
-    class Cutscenes
-    {
-        class Main_Menu_Intro
-        {
-            directory = "z\crusader\addons\utilities\assets\mainmenu.VR"; // Path to scenario with the scene
-        };
+// allow HTML load URLs
+class CfgCommands {
+    allowedHTMLLoadURIs[] += {
+        "https://puotek.github.io/*"
     };
 };
